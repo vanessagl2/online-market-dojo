82fb0e80c8504c5975cf9590f84ef7439de55355
Vanessa Lima <1123066+vanessagl2@users.noreply.github.com>
2019-10-25 15:42:46 -0300

    Remove login/password

--- src/main/java/com/vanessagl2/dojo/market/product/controller/ProductController.java
+++ src/main/java/com/vanessagl2/dojo/market/product/controller/ProductController.java
@@ -3,9 +3,13 @@ package com.vanessagl2.dojo.market.product.controller;
 import com.vanessagl2.dojo.market.product.model.Name;
 import com.vanessagl2.dojo.market.product.model.Price;
 import com.vanessagl2.dojo.market.product.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ProductController {
 
+  @GetMapping(value = "/products")
   public Product findAllProducts() {
     Name expectedName = Name.builder().fullName("Full Product Name").build();
     Price expectedPrice = Price.builder().amount("10.00").currency("USD").build();
